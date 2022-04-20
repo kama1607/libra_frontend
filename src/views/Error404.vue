21084877a56a2c104010f4283c2ecdbdd45ea950
kama1607 <kaxa.ru77@icloud.com>
2022-04-20 19:06:04 +0700

    Страница ошибки

---
+++ src/views/Error404.vue
@@ -0,0 +1,22 @@
+<template>
+    <errorTitle
+    title="Ошибка 404"
+    :text="message"
+    />
+</template>
+
+<script>
+import errorTitle from "../components/errorTitle"
+
+export default {
+    name: "Error404",
+    components: {
+        errorTitle
+    },
+    data: () => ({
+        message: "Адрес не найден"
+    })
+}
+
+
+</script>
